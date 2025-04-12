844b9d509246a8811c2d5afbef6e4efd1b60a74c
Hashi-Rama-1 <seshuaniruddha@gmail.com>
2025-04-12 13:31:12 +0530

    Updates server.js

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import {connectDB} from './config/db.js';
-
+import watchlistRoutes from './routes/watchlist_routes.js';
 
 
 dotenv.config({ path: '../.env' });
 const app = express();
 
-app.use(express.json());
+app.use(express.json());    // Allows to accept JSON data in the body
 app.use(cors({credentials: true, origin: 'http://localhost:5173'}));
+
+
+app.use('/api/watchlist',watchlistRoutes);
 
 
 app.listen(5000, () => {
